81190e59cf36bbb5f13a235c0611de26367dbc9a
Jacinta Chen <jacinta.chen8@gmail.com>
2023-04-26 11:18:58 +1000

    update

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -65,11 +65,23 @@ const Skill = () => {
           <p className="title">
             ABOUT <span>ME</span>
           </p>
-          <p className="introduction">
-            I am an experienced Front End Developer with strong learning ability
-            and have willingness to learn new skills. Like coding and passion
-            for creativity.
-          </p>
+          <div className="introduction">
+            <p>
+              {" "}
+              Hi there! I'm a passionate Front End Developer with a focus on
+              delivering high-quality and user-friendly websites. I'm excited to
+              share my portfolio with you, where you'll find some of my work
+              that showcases my skills and expertise in this field.
+            </p>
+            <p>
+              I'm always eager to learn and explore new technologies and trends,
+              and I understand that every project is an opportunity to grow and
+              improve. With a commitment to creating stunning and responsive
+              designs that meet the needs of end-users, I'm dedicated to
+              delivering results that exceed expectations.
+            </p>
+          </div>
+
           <div className="effect">
             <div className="loop-wrap">{loopItems}</div>
             <div className="fade"></div>
